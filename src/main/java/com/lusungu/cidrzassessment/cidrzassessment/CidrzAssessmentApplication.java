f15548d6fdbe5a603fb8dbe626824e59179b509a
Lusungu Chihana <lusungue@live.co.uk>
2021-07-17 13:19:55 +0200

    initial files

---
+++ src/main/java/com/lusungu/cidrzassessment/cidrzassessment/CidrzAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.lusungu.cidrzassessment.cidrzassessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CidrzAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CidrzAssessmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lusungu/cidrzassessment/cidrzassessment/CidrzAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lusungu.cidrzassessment.cidrzassessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CidrzAssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
